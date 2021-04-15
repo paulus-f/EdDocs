23bce289190d92023b4f8e099f8b91638ed2bb6a
PFdev <fedorovich2012bel@gmail.com>
2021-04-16 00:21:55 +0300

    Add list of questioins

--- app/javascript/components/QuizEditor.jsx
+++ app/javascript/components/QuizEditor.jsx
@@ -10,17 +10,29 @@ import Functions from '../utils/Functions';
 import Chip from '@material-ui/core/Chip';
 import { withRouter, Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+
 
 const styles = theme => ({
   container: {
     display: 'flex',
     flexWrap: 'wrap',
+    maxWidth: '80%'
   },
   textField: {
     width: '160%',
     marginLeft: theme.spacing.unit,
     marginRight: theme.spacing.unit,
-  }
+  },
+  card: {
+    minWidth: 275,
+  },
 });
 
 class QuizEditor extends React.Component {
@@ -121,6 +133,9 @@ class QuizEditor extends React.Component {
       authenticity_token: Functions.getMetaContent("csrf-token")
     })
       .then(res => {
+        this.setState((prevState, props) => {
+          return { quizQuestions: [...prevState.quizQuestions, res.data.quiz_question] }
+        })
         console.log(res)
       })
   }
@@ -137,8 +152,8 @@ class QuizEditor extends React.Component {
     const { quizNewQuestion, } = this.state;
     const { classes } = this.props;
 
-    return <Grid item>
-      <form className={classes.container} autoComplete="off" onSubmit={this.handleQuizSubmit} >
+    return <Grid container>
+      <form autoComplete="off" onSubmit={this.handleQuizSubmit} >
         <FormControl component="fieldset" className={classes.formControl}>
           <TextField
             id="outlined-foundation-name"
@@ -221,7 +236,35 @@ class QuizEditor extends React.Component {
   }
 
   renderQuestions = () => {
+    const { classes } = this.props;
+    const { quizQuestions } = this.state;
 
+    return quizQuestions.map((question) => {
+      return <Grid key={question.id} xs item>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography component="h2">
+              Prompt: {question.prompt}
+            </Typography>
+            <Typography variant='h6' component="h4">
+              A: {question.a}
+            </Typography>
+            <Typography variant='h6' component="h4">
+              B: {question.b}
+            </Typography>
+            <Typography variant='h6' component="h4">
+              C: {question.c}
+            </Typography>
+            <Typography variant='h6' component="h4">
+              D: {question.d}
+            </Typography>
+            <Typography component="h3">
+              Answer: {question.asnwer}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    });
   }
 
   render() {
@@ -229,54 +272,55 @@ class QuizEditor extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div id='managerCalls'>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <FormControl>
-            <TextField
-              autoFocus
-              id="outlined-group-name-input"
-              className={classes.textField}
-              value={this.state.quizName}
-              name="name"
-              required
-              label='Quiz Name'
-              margin="normal"
-              onChange={this.handleChange('name')} />
-            <TextField
-              select
-              label="Select"
-              className={classes.textField}
-              value={this.state.selectedCourse}
-              onChange={this.handleChange('course')}
-              margin="normal"
-              required
-              variant="outlined"
-            >
-              {courses.map(course => (
-                <MenuItem key={course.id} value={course.id}>
-                  {course.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            <Button type="submit" color="primary" aria-label="Add">
-              <AddIcon />
-            </Button>
-          </FormControl>
-        </form>
-        <br />
-        <div>
+      <div id='quizEditor' className={classes.container}>
+        <div className={classes.container} >
+          <form autoComplete="off" onSubmit={this.handleSubmit}>
+            <FormControl>
+              <TextField
+                autoFocus
+                id="outlined-group-name-input"
+                className={classes.textField}
+                value={this.state.quizName}
+                name="name"
+                required
+                label='Quiz Name'
+                margin="normal"
+                onChange={this.handleChange('name')} />
+              <TextField
+                select
+                label="Select"
+                className={classes.textField}
+                value={this.state.selectedCourse}
+                onChange={this.handleChange('course')}
+                margin="normal"
+                required
+                variant="outlined"
+              >
+                {courses.map(course => (
+                  <MenuItem key={course.id} value={course.id}>
+                    {course.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <Button type="submit" color="primary" aria-label="Add">
+                <AddIcon />
+              </Button>
+            </FormControl>
+          </form>
+        </div>
+        <Grid container spacing={3}>
           {quizzes.map((quiz) => {
             return (
-              <Chip key={quiz.id}
-                label={quiz.name}
-                clickable
-                onClick={(e) => this.handleChipClick(quiz.id)}
-                color='primary' />
+              <Grid key={quiz.id} xs item>
+                <Chip label={quiz.name}
+                  clickable
+                  onClick={(e) => this.handleChipClick(quiz.id)}
+                  color='primary' />
+              </Grid>
             );
           })}
-        </div>
-        <br />
-        <Grid item xs={12} container justify="center" spacing={24}>
+        </Grid>
+        <Grid container spacing={3}>
           {selectedQuiz && this.renderNewQuestion()}
           {selectedQuiz && this.renderQuestions()}
         </Grid>
